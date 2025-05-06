be75bc49d0a61adf9466a07ac7a6202f760f7cf3
Christopher Hart <christopherjhart95@gmail.com>
2025-05-05 20:35:28 -0400

    feat: introduce RoomType literal and enhance Message model with personId attribute

--- webexpythonsdk-stubs/webexpythonsdk/models/immutable.pyi
+++ webexpythonsdk-stubs/webexpythonsdk/models/immutable.pyi
@@ -1,9 +1,11 @@
 from datetime import datetime
+from typing import Literal
 
 class Message:
     id: str
     roomId: str  # noqa: N803, N815
     personEmail: str  # noqa: N803, N815
+    personId: str  # noqa: N803, N815
     text: str
     created: datetime | None
 
@@ -12,6 +14,9 @@ class Person:
     displayName: str  # noqa: N803, N815
     emails: list[str]
 
+RoomType = Literal["direct", "group"]
+
 class Room:
     id: str
     title: str
+    type: RoomType
